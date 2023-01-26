260a11bb54625d4cee85c90e5e936cede9c351b9
kimi1723 <100174940+kimi1723@users.noreply.github.com>
2023-01-26 02:13:34 +0100

    final

--- kurs cz3/9 flex project/js/main.js
+++ kurs cz3/9 flex project/js/main.js
@@ -1,9 +1,14 @@
 const navItems = document.querySelector('.nav-items');
 const burgerBtn = document.querySelector('.burger-btn');
 const navLinks = document.querySelectorAll('.nav__item');
+const navBtnBars = document.querySelector('.burger-btn__bars');
+const allSections = document.querySelectorAll('.section');
+const footerYear = document.querySelector('.footer__year');
 
 const handleNav = () => {
 	navItems.classList.toggle('nav-items--active');
+
+	navBtnBars.classList.remove('black-bars-color');
 
 	handleNavItemsAnimation();
 };
@@ -18,5 +23,27 @@ const handleNavItemsAnimation = () => {
 	});
 };
 
+// const handleObserver = () => {
+// 	const currentSection = window.scrollY;
+
+// 	allSections.forEach(section => {
+// 		console.log(section.offsetTop);
+// 		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+// 			navBtnBars.classList.add('black-bars-color');
+// 		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+// 			navBtnBars.classList.remove('black-bars-color');
+// 		}
+// 	});
+// };
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+
+	footerYear.textContent = `${year} `;
+};
+
+// handleObserver();
+handleCurrentYear();
 navLinks.forEach(link => link.addEventListener('click', handleNav));
 burgerBtn.addEventListener('click', handleNav);
+window.addEventListener('scroll', handleObserver);
