e3ff11c8de5bd0baf4d7db81ef4557e3372f3d34
kimi1723 <100174940+kimi1723@users.noreply.github.com>
2023-01-31 23:54:23 +0100

    gulp config

---
+++ kurs cz4/strona1/src/js/main.js
@@ -0,0 +1,7 @@
+const h2 = document.querySelector('h1');
+
+h2.textContent = 'aaaaaaaaaaaa';
+
+// Żeby najnowsza wersja kita działała trzeba w pliku index.js dopisać definicję zmiennej (pewnie przypadkowo usunięta):
+// const gutil = require('gulp-util');
+// plik znajduje się w katalogu \node_modules\gulp-kit
