b0373fa2a7704e71c575430b50e48d9e777297df
kimi1723 <100174940+kimi1723@users.noreply.github.com>
2023-02-03 03:36:36 +0100

    aside, offer sections

--- zlecenie od klienta/src/js/main.js
+++ zlecenie od klienta/src/js/main.js
@@ -1,11 +1,26 @@
 const navBtn = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav__links');
 const navLinks = document.querySelectorAll('.nav__links-link');
+const offerBtns = document.querySelectorAll('.cards-box__button');
+let i = 0;
 
 const handleNav = () => {
 	navBtn.classList.toggle('is-active');
 	navMenu.classList.toggle('show-menu');
 };
 
+// NA FOCUS TRZEBA TO NAPISAC
+const handleBtns = () => {
+	if (i === 0) {
+		console.log(i);
+		const activeBtn = document.querySelector('.cards-box__button--active');
+
+		activeBtn.classList.remove('cards-box__button--active');
+		i++;
+	}
+	// lub offerbtns.foreach btn => btn.classlist.remove('cards-box__button--active')
+};
+
+offerBtns.forEach(btn => btn.addEventListener('click', handleBtns));
 navLinks.forEach(link => link.addEventListener('click', handleNav));
 navBtn.addEventListener('click', handleNav);
