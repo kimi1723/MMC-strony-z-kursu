863e5f0361bd8fbfccbc788da4d1dea10820a51e
kimi1723 <100174940+kimi1723@users.noreply.github.com>
2023-02-08 03:54:41 +0100

    final commit

--- zlecenie od klienta/src/js/main.js
+++ zlecenie od klienta/src/js/main.js
@@ -9,6 +9,8 @@ const activeBtn = document.querySelector('.cards-box__button--active');
 const btnEventListeners = ['click', 'keydown'];
 let i = 0;
 
+const scrollSpySections = document.querySelectorAll('.scrollspy-section');
+
 const handleYear = () => {
 	const year = new Date().getFullYear();
 
@@ -33,9 +35,30 @@ const handleBtns = () => {
 	);
 };
 
+const handleScrollSpy = () => {
+	if (document.body.classList.contains('main-page')) {
+		const sections = [];
+
+		scrollSpySections.forEach(section => {
+			if (window.scrollY <= section.offsetTop + section.offsetHeight - 112) {
+				sections.push(section);
+
+				const activeSection = navMenu.querySelector(`[href*="${sections[0].id}"]`);
+
+				navLinks.forEach(link => link.classList.remove('nav__links-link--active'));
+
+				activeSection.classList.add('nav__links-link--active');
+			}
+		});
+	}
+};
+
 handleYear();
 navBtn.addEventListener('click', handleNav);
 navLinks.forEach(link => link.addEventListener('click', handleNav));
 btnEventListeners.forEach(e => {
 	window.addEventListener(e, handleBtns);
 });
+
+window.addEventListener('scroll', handleScrollSpy);
+window.addEventListener('DOMContentLoaded', handleScrollSpy);
